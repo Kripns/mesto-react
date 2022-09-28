44b306adba20eb97a1fc0b7c2bd2a454ad9673c9
Peter Sidorov <kripns06@gmail.com>
2022-09-29 00:30:08 +1200

    hide remove icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,12 @@
+import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
 function Card(props) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const isOwn = currentUser._id === props.card.owner._id;
+  const cardRemoveIconClassName = `place-card__remove-icon ${!isOwn ? 'place-card__remove-icon_hidden' : null}`;
+  
+
   function handleClick() {
     props.onCardClick(props.card);
   }
@@ -13,7 +21,7 @@ function Card(props) {
       />
       <div className='place-card__info'>
         <h2 className='place-card__heading'>{props.card.name}</h2>
-        <button className='place-card__remove-icon' type='button' />
+        <button className={cardRemoveIconClassName} type='button' />
         <div className='place-card__likes-wrapper'>
           <button className='place-card__like' type='button' />
           <p className='place-card__like-counter'>{props.card.likes.length}</p>
